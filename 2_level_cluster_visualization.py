ebfa4f75d42f88110c0501e7b9095c5ab570578e
samin <sample@gmail.com>
2019-11-08 12:32:45 +0100

    cleaned code

--- 2_level_cluster_visualization.py
+++ 2_level_cluster_visualization.py
@@ -2,80 +2,83 @@ import numpy as np
 import pandas as pd
 from os import path
 from PIL import Image
-from wordcloud import WordCloud, STOPWORDS, ImageColorGenerator
-from nltk.tokenize import word_tokenize
+from wordcloud import WordCloud, ImageColorGenerator
+#from nltk.tokenize import word_tokenize
+### tokenize ###
+import spacy
+### stopwords ###
+from spacy.lang.en.stop_words import STOP_WORDS
+
 import collections
 import matplotlib.pyplot as plt
 
-def filter(text, sub_str):
-    ret = [str.replace("d'","").replace("l'","").replace('"',"").replace("'","").replace(",","") for str in text if
-            str not in sub_str] 
-    return ret
+import argparse 
 
-def duplicate(input_list, n):
-    new_list = []
-    str_list = ' '.join(map(str,input_list))
-    #for i in range(n):
-    #    new_list.append(str_list)
-    #return new_list 
-    return str_list
+
+args = argparse.ArgumentParser("input arguments")
+args.add_argument("--corpus_name", required= True, default="fr_core_news_sm",\
+     type=str, help="insert the pretrained statistical corpus name, default is for French, fr_core_news_sm")
+args = parser.parse_args()
+
+nlp = spacy.load(args.corpus_name)
+
 df = pd.read_csv("../clustering_result_SG_2l.csv", sep = '\t', header = None, names = ["query","cluster","intent","sub_cluster","cluster_words","subcluster_words"])
-#print(df.head(5))
-#i=0
-#while i<= 5:
-stopwords = ['un', 'une', 'le', 'les', 'la', 'de', 'de la', 'du', 'des', 'en', "l'",\
-    'a', "a'", 'pour', 'à', 'au', 'on', 'et']
 
-       
-
-#df['cluster_words'] = np.nan
-#df['subcluster_words'] = np.nan
 for cluster in df["cluster"].unique():
     df_new = df[df["cluster"]==cluster]
-    tokens = df_new["query"].apply(word_tokenize)
-    all_t = [" ".join(filter(text,stopwords)) for text in tokens]
+    
+    ### Tokenize and filter stop-words using nltk
+    #tokens = df_new["query"].apply(word_tokenize)
+    #all_t = [" ".join(filter(text,stopwords)) for text in tokens]
+    
+    ### Tokeniz, lemmatize and remove stop-words using Spacy 
+    tokens = df_new["query"].apply(nlp)
+    tokens =  map(lambda text: map(lambda x: x.lemma_, text), tokens)
+    all_t = [" ".join(map(lambda x: str(x) if not nlp.vocab[str(x)].is_stop else "", text)) for text in tokens]
     all_t = ' '.join(map(str,all_t))
-    wordcloud = WordCloud(stopwords=stopwords, collocations=False).generate(all_t)
+
+    wordcloud = WordCloud(collocations=False).generate(all_t)
     plt.imshow(wordcloud, interpolation='bilinear')
     plt.title(cluster)
     plt.axis("off")
     plt.show()
     
+
     filtered_words = [word for word in str(all_t).split()]
     counted_words = collections.Counter(filtered_words)
 
     cluster_words = []
     counts = []
+    nc_kw = 2
+    nsc_kw = 2 
     cluster_most_frequent_word = ''
-    for letter, count in counted_words.most_common(3):
+    for letter, count in counted_words.most_common(nc_kw):
         if cluster_most_frequent_word=='':
             cluster_most_frequent_word = letter
         cluster_words.append(letter)
         counts.append(count)
     print (f"cluster {cluster}'s main vocabs:{cluster_words} with frenquncy of {counts}")
-    #to_save = duplicate(words,df_new.shape[0])
-    #to_ = np.reshape(to_save,[len(to_save),1])
-    #print (len(to_save))
-    #print (np.reshape(to_save,[len(to_save),1]).T)
+    
     df.at[df["cluster"]==cluster , 'cluster_words'] = ' '.join(map(str,cluster_words))
     print(df[df["cluster"]==cluster]['cluster_words'].notnull().unique())
     for sub_cluster in df_new["sub_cluster"].unique():
         df_sub = df_new[df_new["sub_cluster"]==sub_cluster]
-        tokens = df_sub["query"].apply(word_tokenize)
-        all_t = [" ".join(filter(text,stopwords)) for text in tokens]
-        cluster_len = len(tokens)
+        tokens = df_sub["query"].apply(nlp)
+        tokens =  map(lambda text: map(lambda x: x.lemma_, text), tokens)
+    
+        all_t = [" ".join(map(lambda x: str(x) if not nlp.vocab[str(x)].is_stop else "", text)) for text in tokens]
+        cluster_len = len(df_sub["query"])
         all_t = ' '.join(map(str,all_t))
         filtered_words = [word for word in str(all_t).split() if word not in cluster_words]
         counted_words = collections.Counter(filtered_words)
 
         words = []
         counts = []
-        for letter, count in counted_words.most_common(3): 
+        for letter, count in counted_words.most_common(nsc_kw): 
             words.append(letter)
             counts.append(count)
         print (f"{cluster_len} :cluster {cluster}:sub-cluster {sub_cluster}'s main vocabs:{words} with frenquncy of {counts}")
         r = df[(df["cluster"]==cluster) & (df['sub_cluster']==sub_cluster)].shape[0]
         df.at[(df["cluster"]==cluster) & (df['sub_cluster']==sub_cluster),'subcluster_words'] = ' '.join(map(str,words)) # duplicate([words],df_sub.shape[0])
 df.to_csv("../named_clusters.csv")
-
 
--- 2level_clustering.py
+++ 2level_clustering.py
@@ -1,48 +1,78 @@
 import csv
 from bert_serving.client import BertClient
 from sklearn.cluster import KMeans
+from sklearn.cluster import DBSCAN
 from yellowbrick.cluster import KElbowVisualizer
+import argparse 
 
-#for item in vs1[0][:]:
- #   print(item)
 
+args = argparse.ArgumentParser("input arguments")
+args.add_argument("--corpus_name", required= True, default="fr_core_news_sm",\
+     type=str, help="insert the pretrained statistical corpus name, default is for French, fr_core_news_sm")
+args = parser.parse_args()
+
+### tokenize ###
+import spacy
+nlp = spacy.load(args.corpus_name) 
+### stopwords ###
+from spacy.lang.en.stop_words import STOP_WORDS
 
 import numpy as np
-#For clustering 
 import pandas as pd
 
 df = pd.read_csv("../data/bank_SG.csv")
 query_list = df["query"].tolist()
 
+no_clusters = 10
+no_subclusters = 10
 
 bc = BertClient(ip='localhost')
+
+### Tokeniz, lemmatize and remove stop-words using Spacy 
+tokens = df["query"].apply(nlp)
+tokens =  map(lambda text: map(lambda x: x.lemma_, text), tokens)
+query_list = [" ".join(map(lambda x: str(x) if not nlp.vocab[str(x)].is_stop else "", text)) for text in tokens]
+    
 BERT_embedding = bc.encode(query_list)
 
 
 km = KMeans()
-km = KMeans(n_clusters=7)
+km = KMeans(n_clusters=no_clusters)
 km.fit(BERT_embedding)
 clusters = km.labels_.tolist()
-
-df_query_cluster = pd.DataFrame({'query':query_list,
+'''
+### testing the accuracy of DBScan
+dbscan1 = DBSCAN(eps= 8, min_samples=4,).fit(BERT_embedding)
+clusters = dbscan1.labels_.tolist()
+'''
+df_query_cluster = pd.DataFrame({'query':df["query"].tolist(),
                                 'cluster': clusters,
                                 'intent': df["Intention"].tolist(),
                                 'sub_cluster':np.nan })
-#df_query_cluster['sub_cluster'] = np.nan                        
 for i in set(clusters):
     cluster_query_list = df_query_cluster[df_query_cluster['cluster'] == i]['query']
-    cluster_BERT_embedding = bc.encode(cluster_query_list.to_list())
-    km = KMeans(n_clusters=10)
+    
+    ### Tokeniz, lemmatize and remove stop-words using Spacy 
+    tokens = cluster_query_list.apply(nlp)
+    tokens =  map(lambda text: map(lambda x: x.lemma_, text), tokens)
+    cluster_query_list = [" ".join(map(lambda x: str(x) if not nlp.vocab[str(x)].is_stop else "", text)) for text in tokens]
+    
+    
+    cluster_BERT_embedding = bc.encode(cluster_query_list)
+    
+    km = KMeans(n_clusters=no_subclusters)
     km.fit(cluster_BERT_embedding)
-    #visualizer = KElbowVisualizer(km, k=(2,7))
-    #visualizer.fit(cluster_BERT_embedding)
-    #visualizer.show()
     clusters = km.labels_.tolist()
+    '''
+    ### testing the accuracy of DBScan
+    dbscan = DBSCAN(eps=8, min_samples=3).fit(cluster_BERT_embedding)
+    clusters = dbscan.labels_.tolist()
+    '''
+
     print ('clusters:',len(clusters))
     print ('query:',len(cluster_query_list))
     df_query_cluster.at[df_query_cluster['cluster'] == i,'sub_cluster'] = clusters
-# for 2-20 the best number of clueters id 7,8 
-# and for 2-40 the best is 18
+
 print(df_query_cluster.shape)
 df_query_cluster.to_csv("../clustering_result_SG_2l.csv", sep = '\t', index = False, header = False)
 
